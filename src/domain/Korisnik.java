ad4f7d58aaeebbdf0f5c62813ed57a68e752aed4
Nino Vidanovic <bino.94@hotmail.com>
2022-02-25 12:21:29 +0100

    Changes to Korisnik and role class

--- src/domain/Korisnik.java
+++ src/domain/Korisnik.java
@@ -1,45 +1,23 @@
 package domain;
 
-public class Korisnik {
+public abstract class Korisnik {
 
 	private final String ime;
 	private final String prezime;
 	private final String username;
 	private final String password;
-	private final Role role;
 	
-	public static Korisnik newKorisnik(String ime, String prezime, String userName, String passWord, Role role) {
-		return new Korisnik(ime,prezime,userName,passWord,role);
-	}
-	
-	private Korisnik(String ime, String prezime, String username, String password, Role role) {
+	public Korisnik(String ime, String prezime, String username, String password) {
 		super();
 		this.ime = ime;
 		this.prezime = prezime;
 		this.username = username;
 		this.password = password;
-		this.role = role;
-	}
-	
-	public String getIme() {
-		return ime;
-	}
-	public String getPrezime() {
-		return prezime;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public Role getRole() {
-		return role;
 	}
 
 	@Override
 	public String toString() {
-		return "Korisnik [ime=" + ime + ", prezime=" + prezime + ", username=" + username + ", role=" + role + "]";
+		return "Korisnik [ime=" + ime + ", prezime=" + prezime + ", username=" + username + "]";
 	}
 	
 }
--- src/domain/Role.java
+++ src/domain/Role.java
@@ -2,7 +2,12 @@ package domain;
 
 public enum Role {
 
-	ADMIN,
-	EDITOR;
+	ADMIN("Admin"),
+	EDITOR("Editor");
 	
+	public final String role;
+	
+	private Role(String role) {
+		this.role = role;
+	}
 }
